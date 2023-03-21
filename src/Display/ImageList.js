d5e53474a1b575db95cabce4ecd814bb20eec5c7
BenDaBeast22 <53120446+BenDaBeast22@users.noreply.github.com>
2023-03-20 22:16:17 -0400

    finished image modal

--- src/Display/ImageList.js
+++ src/Display/ImageList.js
@@ -1,17 +1,13 @@
-import React, { useState } from "react";
+import React, { useState, useEffect, useCallback } from "react";
 import {
-  Grid,
   Card,
   CardMedia,
-  CardContent,
   Box,
-  Typography,
-  Paper,
-  Button,
   ImageList as ImgList,
   ImageListItem,
   ImageListItemBar,
   Modal,
+  Container,
 } from "@mui/material";
 import EditCardIcon from "../Icons/EditCardIcon";
 import DeleteCardIcon from "../Icons/DeleteCardIcon";
@@ -20,29 +16,54 @@ import Carousel from "react-material-ui-carousel";
 function ImageList({ list, editCard, EditCardDialog, handleDelete, type }) {
   const [dialogOpen, setDialogOpen] = useState(false);
   const [dialogIndex, setDialogIndex] = useState(0);
+
   const handleOpenDialog = (index) => {
-    console.log(index);
     setDialogIndex(index);
     setDialogOpen(true);
   };
   const handleCloseDialog = () => {
     setDialogOpen(false);
   };
+  const handleKeydown = useCallback(
+    (event) => {
+      // left key
+      if (event.keyCode === 37) {
+        setDialogIndex((index) => {
+          return index - 1 < 0 ? list.length - 1 : index - 1;
+        });
+        // right key
+      } else if (event.keyCode === 39) {
+        setDialogIndex((index) => {
+          return index + 1 > list.length - 1 ? 0 : index + 1;
+        });
+      }
+    },
+    [list]
+  );
+  useEffect(() => {
+    window.addEventListener("keydown", handleKeydown);
+    return () => window.removeEventListener("keydown", handleKeydown);
+  });
   return (
     <>
-      <ImgList gap={15} cols={5} rowHeight={150}>
+      <ImgList gap={15} cols={5}>
         {list.map((card, index) => (
-          <ImageListItem key={card.id}>
-            {/* <Card
+          <ImageListItem sx={{ height: "100%" }} key={card.id}>
+            <Card
               sx={{
-                height: "150px",
+                height: "200px",
                 display: "flex",
                 justifyContent: "center",
               }}
             >
-              <CardMedia height="100%" component="img" image={card.img} alt={card.img} />
-            </Card> */}
-            <img src={card.img} alt={card.img} onClick={() => handleOpenDialog(index)} />
+              <CardMedia
+                height="100%"
+                component="img"
+                image={card.img}
+                alt={card.img}
+                onClick={() => handleOpenDialog(index)}
+              />
+            </Card>
             <ImageListItemBar
               title={card.name}
               actionIcon={
@@ -69,75 +90,41 @@ function ImageList({ list, editCard, EditCardDialog, handleDelete, type }) {
             left: "50%",
             transform: "translate(-50%, -50%)",
             bgcolor: "background.paper",
-            border: "2px solid #000",
             boxShadow: 24,
           }}
         >
-          <Carousel index={dialogIndex} autoPlay={false} sx={{ height: "800px", width: 1200 }}>
-            {list.map((card) => (
-              <ImageListItem key={card.id} sx={{ width: 500 }}>
-                <Card
-                  sx={{
-                    height: "700px",
-                    display: "flex",
-                    justifyContent: "center",
-                  }}
-                >
-                  <CardMedia height="100%" component="img" image={card.img} alt={card.img} />
-                </Card>
-
-                {/* <img src={card.img} alt={card.img} sx={{ height: "100%" }} /> */}
-                <ImageListItemBar
-                  title={card.name}
-                  actionIcon={
-                    <EditCardIcon card={card} editCard={editCard} EditCardDialog={EditCardDialog} type={type} />
-                  }
+          <Carousel
+            index={dialogIndex}
+            next={() => setDialogIndex((index) => index + 1)}
+            prev={() => setDialogIndex((index) => index - 1)}
+            autoPlay={false}
+            duration={250}
+            navButtonsAlwaysVisible
+            sx={{ height: "750px", width: "1200px" }}
+          >
+            {list.map((card, i) => (
+              <Container
+                key={i}
+                sx={{
+                  display: "flex",
+                  justifyContent: "center",
+                  alignItems: "center",
+                  width: "1200px",
+                  height: "700px",
+                }}
+              >
+                <img
+                  src={card.img}
+                  alt={card.img}
+                  style={{ objectFit: "contain", maxHeight: "650px", maxWidth: "1200px" }}
+                  loading="lazy"
                 />
-              </ImageListItem>
+              </Container>
             ))}
           </Carousel>
         </Box>
       </Modal>
     </>
-
-    // <Grid container spacing={2} sx={{ pb: 2, pr: 2 }}>
-    //   {list.map((card) => (
-    //     <Grid item xs={4} sm={3} md={2} key={card.id}>
-    //       <Card
-    //         sx={{
-    //           height: "150px",
-    //           display: "flex",
-    //           display: "flex",
-    //           justifyContent: "center",
-    //         }}
-    //       >
-    //         <CardMedia height="100%" component="img" image={card.img} alt={card.img} />
-    //       </Card>
-    //       <CardContent
-    //         sx={{
-    //           py: 1,
-    //           backgroundColor: "#121212",
-    //           "&:last-child": {
-    //             paddingBottom: 1,
-    //           },
-    //           backgroundImage: "linear-gradient(rgba(255, 255, 255, 0.09), rgba(255, 255, 255, 0.09))",
-    //         }}
-    //       >
-    //         <Typography align="center">{card.name}</Typography>
-    //         <Box
-    //           sx={{
-    //             my: 1,
-    //             display: "flex",
-    //             justifyContent: "space-evenly",
-    //           }}
-    //         >
-    //           <EditCardIcon card={card} editCard={editCard} EditCardDialog={EditCardDialog} type={type} />
-    //           <DeleteCardIcon handleDelete={handleDelete} type={type} card={card} />
-    //         </Box>
-    //       </CardContent>
-    //     </Grid>
-    //   ))}
-    // </Grid>
   );
 }
 
