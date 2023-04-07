467ca1bfeb321e49218a9b8b24db9b8b40a3fc6c
BenDaBeast22 <53120446+BenDaBeast22@users.noreply.github.com>
2023-04-07 10:50:36 -0400

    test

--- src/Config/firebase.js
+++ src/Config/firebase.js
@@ -12,7 +12,16 @@ import {
   signOut,
   updateProfile,
 } from "firebase/auth";
-import { firebaseConfig } from "./firebase.config";
+
+const firebaseConfig = {
+  apiKey: process.env.REACT_APP_FIREBASE_API_KEY,
+  authDomain: process.env.REACT_APP_AUTH_DOMAIN,
+  projectId: process.env.REACT_APP_PROJECT_ID,
+  storageBucket: process.env.REACT_APP_STORAGE_BUCKET,
+  messagingSenderId: process.env.REACT_APP_MESSAGING_SENDER_ID,
+  appId: process.env.REACT_APP_APP_ID,
+  measurementId: process.env.REACT_APP_MEASUREMENT_ID,
+};
 
 // Initialize Firebase and Firestore
 const app = initializeApp(firebaseConfig);
